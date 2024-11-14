d667c9ab59be407c769294f5e7c93b56ed359d5c
Leiweibo <279605106@qq.com>
2024-11-14 22:24:38 +0800

    东财日线数据爬取

--- data/internal/crawler/crawler.go
+++ data/internal/crawler/crawler.go
@@ -1,10 +1,8 @@
 package crawler
 
-import "github.com/rayjiu/quantt/data/internal/db/service"
-
 func Start() {
-	service.SecotorService.RefreshCache()
-	sectorBaseInfoCrawler.startCrawSectorInfo()
-	ffCrawler.startHistoryFundFlowData()
-	kCrawler.startCrawlKlineData("601318", 1)
+	// service.SecotorService.RefreshCache()
+	// sectorBaseInfoCrawler.startCrawSectorInfo()
+	// ffCrawler.startHistoryFundFlowData()
+	kCrawler.startCrawlKlineData("515170", 1)
 }
